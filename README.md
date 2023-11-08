e0910e5e63475ad553d88ed4b6ba8f3722d217c8
d1hn1 <daniellopezsantiago27@gmail.com>
2023-11-08 13:49:29 +0100

    Updated

---
+++ README.md
@@ -0,0 +1,3 @@
+# TCHAT
+
+## C++ Simple chat
---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <string>
+#include <vector>
+#include <thread>
+#include <cstring>
+#include <unistd.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define LISTEN_BACKLOG 50
+
+// INADDR_ANY
+// INADDR_LOOPBACK
+
+struct sockaddr_in in_addr;
+sockaddr* peer_ip;
+socklen_t* peer_size;
+
+std::vector<int> CLIENTS;
+
+int sendall( const char *MESSAGE, int actual_client )
+{
+	for ( auto client : CLIENTS) {
+		if ( actual_client != client) {
+			send(client, MESSAGE, sizeof(MESSAGE), 0);
+		};
+	};
+	return 0;
+}
+
+// TODO: Make a function to remove a client from the list
+
+void conn_handler( int client_socket )
+{
+	CLIENTS.push_back(client_socket);
+
+	const char *HELLO_MESSAGE 	= "\nTchat v1.0\nINFO: Type exit to leave \r\n";
+	const char *BYE_MESSAGE		= "\nINFO: User disconnected \r\n";
+
+	bool TERMINATE_RECEIVING = false;
+
+	int hello_message = send(client_socket, HELLO_MESSAGE, strlen(HELLO_MESSAGE), 0);
+
+	if ( hello_message == strlen(HELLO_MESSAGE) ) { 
+		std::cout << "INFO: Sent hello message" << std::endl;
+	};
+	
+	while ( TERMINATE_RECEIVING != true ) {
+		
+		char buff[2048];
+		int bytes = recv(client_socket, buff, 2048, 0);
+		buff[bytes] = '\0';
+		
+		if ( strcoll(buff, "exit") == 13 ) {
+			TERMINATE_RECEIVING = true;
+			sendall(BYE_MESSAGE, client_socket);
+		};
+
+		// TODO: HANDLE HERE THE CHANNELS AND SENDERS
+
+		sendall(buff, client_socket);
+
+	};
+	
+	close(client_socket);
+}
+
+int main()
+{
+	auto PERMISIONS = getuid();
+
+	if ( PERMISIONS != 0 ) {
+		std::cout << "ERROR: Run as root" << std::endl;
+		return 1;
+	};
+
+	in_addr.sin_family = AF_INET;
+	in_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	in_addr.sin_port = htons(1010);
+	
+	int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if ( tcp_socket == -1 ) {
+		std::cout << "ERROR: could not create a socket" << std::endl;
+		exit(1);
+	} 
+	
+	int binding = bind(tcp_socket, (sockaddr*)&in_addr, sizeof(in_addr));
+
+	if ( binding == -1 ) {
+		std::cout << "ERROR: Could not bind to port" << std::endl;
+		exit(1);
+	};
+
+	std::cout << "INFO: Listening on port 1010" << std::endl;
+
+	listen(tcp_socket, LISTEN_BACKLOG);
+
+	while ( true )
+	{
+
+		int new_socket = accept(tcp_socket, peer_ip, peer_size);
+
+		if ( new_socket ) {
+			std::cout << "INFO: Connection accepted" << std::endl;
+		};
+
+		std::thread handling_thread(conn_handler, new_socket);
+		handling_thread.detach();
+
+	};
+};
